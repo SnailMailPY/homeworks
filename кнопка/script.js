59623ba1d52d7a0021ac0a77ac85522e7873d9b9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-01 13:01:39 +0300

    ого

---
+++ кнопка/script.js
@@ -0,0 +1,10 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('mouseenter', () => {
+    btn.style.left = `${random(0, 100)}%`;
+    btn.style.top = `${random(0, 100)}%`;
+});
