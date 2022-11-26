b9ce264a512f80823a95d34bccd11da0d78072ec
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-26 22:18:50 +0300

    перемен... требуют наши сердца....

--- кнопка/script.js
+++ кнопка/script.js
@@ -2,9 +2,10 @@ const random = (min, max) => {
     const rand = min + Math.random() * (max - min + 1);
     return Math.floor(rand);
 }
+var count=1;
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('mouseenter', () => {
-    btn.style.left = `${random(0, 100)}%`;
-    btn.style.top = `${random(0, 100)}%`;
+    btn.style.left = `${random(0, 99)}%`;
+    btn.style.top = `${random(0, 99)}%`;
 });
