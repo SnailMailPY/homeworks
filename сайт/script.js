6120e152d241c04af2a49829d4de8cc2cc18f2e2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-28 14:11:19 +0300

    я хочу пирожок

---
+++ сайт/script.js
@@ -0,0 +1,62 @@
+//document.getElementById("bb").src="/images/2.00.png";
+//const image = document.querySelector('.img');
+//image.setAttribute('src', '\images\2.00.png');
+//document.header.style.backgroundImage = 'https://st.peopletalk.ru/wp-content/uploads/2022/07/2.00.png';
+//document.getElementById('elem').style.backgroundColor = '#FF0000';
+
+
+let checkbox = document.querySelector('.checkbox')
+checkbox.style.backgroundColor = 'rgb(31, 26, 26)'
+checkbox.style.color = 'red'
+
+let customcheckbox = document.querySelector('.custom-checkbox')
+customcheckbox.style.color = 'red'
+
+let manlabel = document.getElementById('manlabel')
+manlabel.style.color = 'rgb(189, 179, 179)'
+
+let womanlabel = document.getElementById('womanlabel')
+womanlabel.style.color = 'rgb(189, 179, 179)'
+
+let childrenlabel = document.getElementById('childrenlabel')
+childrenlabel.style.color = 'rgb(189, 179, 179)'
+
+let button = document.querySelector('.button')
+button.style.backgroundColor = 'rgb(102, 89, 89)'
+button.style.color = 'rgb(209, 192, 192)'
+
+
+let manSection = document.querySelector('.manSection')
+manSection.style.backgroundColor = 'rgb(31, 26, 26)'
+manSection.style.color = 'rgb(189, 179, 179)'
+
+let polosa = document.querySelector('.polosa')
+polosa.style.backgroundColor = 'rgb(189, 179, 179)'
+
+let polosa1 = document.querySelector('.polosa1')
+polosa1.style.backgroundColor = 'rgb(189, 179, 179)'
+
+let stuff = document.querySelector('.stuff')
+stuff.style.backgroundColor = 'rgb(31, 26, 26)'
+
+//let stuffWoman = document.querySelector('.stuffWoman')
+//stuffWoman.style.backgroundColor = 'rgb(69, 60, 60)'
+
+let drobdiv = document.querySelector('.drobdiv')
+drobdiv.style.backgroundColor = 'rgb(31, 26, 26)'
+
+let drob1 = document.querySelector('.drob1')
+drob1.style.backgroundColor = 'rgb(31, 26, 26)'
+
+let womanSection = document.querySelector('.womanSection')
+womanSection.style.backgroundColor = 'rgb(31, 26, 26)'
+womanSection.style.color = 'rgb(189, 179, 179)'
+
+let stuffdown = document.querySelector('.stuffdown')
+stuffdown.style.backgroundColor = 'rgb(69, 60, 60)'
+
+let stuff1 = document.querySelector('.stuff1')
+stuff1.style.backgroundColor = 'rgb(69, 60, 60)'
+
+let drobdiv2 = document.querySelector('.drobdiv2')
+drobdiv2.style.backgroundColor = 'rgb(31, 26, 26)'
